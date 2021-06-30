c6ff593c002914358f9e0b410e96eec776fbe93b
Johan Vos <johan@lodgon.com>
2021-06-30 14:26:41 +0200

    fix typo

--- ch14-JavaFXScience/hellond4j/src/main/java/org/modernclient/HelloNd4j.java
+++ ch14-JavaFXScience/hellond4j/src/main/java/org/modernclient/HelloNd4j.java
@@ -12,7 +12,7 @@ public class HelloNd4j {
 
         INDArray b = Nd4j.create(new double[] {0.,1.,2.,3.,4.,5.}, new int[] {2,3});
         INDArray c = Nd4j.create(new double[] {2.,-1.,3.}, new int[] {3,1});
-        System.out.println("Matrix b has 2 rows ad 3 columns:\n"+b);
+        System.out.println("Matrix b has 2 rows and 3 columns:\n"+b);
         System.out.println("++++++++++++++++++++++++++++++\n");
         System.out.println("Vector c has 3 elements:\n"+c);
         System.out.println("++++++++++++++++++++++++++++++\n");
